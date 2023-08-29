a5a281f2a1309c0d8808c516f947523e565ffd66
william-hu-codes <william.hu307@gmail.com>
2023-08-29 17:06:03 -0400

    add typing css animation to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,10 @@ import "./Home.css"
 
 function Home(props) {
   return (
-    <div className="home-ctr container">
+    <div className="home-ctr">
       <h3 className="intro">Hi there, my name is</h3>
       <h1 className="name">William Hu</h1>
-      <h2 className="title">I am a Fullstack Developer</h2>
+      <h2 className="title typed-out">I am a Software Developer</h2>
       <h3 className="greeting">Nice to e-meet you!</h3>
     </div>
   )
