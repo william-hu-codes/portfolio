92c1b63ab4f575b2a4f13f843c6295acd6117722
william-hu-codes <william.hu307@gmail.com>
2023-08-29 18:08:54 -0400

    add typing animation and included acknowledgement in readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ A brief summary of who you are
 A link to your deployed application
 Technologies used
 Acknowledgements
+https://github.com/brandonmcconnell/typed.css
 References for your inspiration/sources
 A list of future features
 -----------------
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
-import "./Home.css"
+import "./Home.scss"
+
 
 function Home(props) {
   return (
     <div className="home-ctr">
       <h3 className="intro">Hi there, my name is</h3>
       <h1 className="name">William Hu</h1>
-      <h2 className="title typed-out">I am a Software Developer</h2>
+      <h2 className="title">I am a <span className="dynamic-text"></span>!</h2>
+      {/* <h2 className="title input-wrapper typed-out">I am a <span className="input placeholder" >Software Developer</span></h2> */}
       <h3 className="greeting">Nice to e-meet you!</h3>
     </div>
   )
