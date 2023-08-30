a65586dacc9327fbee4ebeb4b61359c7ae95d428
william-hu-codes <william.hu307@gmail.com>
2023-08-29 20:20:08 -0400

    new color theme again

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,23 @@
 import "./Footer.css"
 import images from "../../assets/images"
+import { FaLinkedin, FaGithub, FaMailBulk } from "react-icons/fa"
+
 
 function Footer(props) {
   console.log(images)
+  
     return (
       <footer className="flex-ctr-ctr">
           <h4>Connect with me!</h4>
           <div className="social-media-ctr">
             <a href="https://www.linkedin.com/in/william-wl-hu" target="_blank" >
-              <img className="social-media-icon" src={images.linkedInIcon} />
+              <FaLinkedin style={{color: "black"}} size={25}/>
             </a>
             <a href="https://github.com/william-hu-codes" target="_blank" >
-              <img className="social-media-icon" src={images.gitHubIcon} />
+              <FaGithub style={{color: "black"}} size={25} />
             </a>
             <a href="mailto:william.hu307@gmail.com">
-              <img className="social-media-icon" src={images.emailIcon} />
+             <FaMailBulk style={{color: "black"}} size={25} />
             </a>
           </div>
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,16 +8,16 @@ export default function Header(props) {
           {/* <h1>My Portfolio</h1> */}
           <nav>
             <Link className="link logo-ctr" to="/" >
-              <img className="logo" src="https://i.imgur.com/FnTxhIy.png" alt="William Hu Logo" />
+              <img className="logo" src="https://i.imgur.com/JqRF8Em.png" alt="William Hu Logo" />
             </Link>
             <Link className="link" to="/">
-              <h3 className="nav-text" >HOME</h3>
+              <h3 className="nav-text" >// HOME</h3>
             </Link>
             <Link className="link" to="/about">
-              <h3 className="nav-text" >ABOUT</h3>
+              <h3 className="nav-text" >// ABOUT</h3>
             </Link>
             <Link className="link" to="/projects">
-              <h3 className="nav-text" >PROJECTS</h3>
+              <h3 className="nav-text" >// PROJECTS</h3>
             </Link>
           </nav>
         </header>
