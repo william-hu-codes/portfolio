3e551ffda0c62d16ab23735ed1dbde34c3d1eb85
william-hu-codes <william.hu307@gmail.com>
2023-09-02 14:18:08 -0400

    link resume download to home button

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,13 @@
-import WavelengthEMR from "./emr.png"
-import SpaceInvaders from "./space-invaders.png"
-import TicTacToe from "./tic-tac-toe.png"
+// import WavelengthEMR from "./emr.png"
+// import SpaceInvaders from "./space-invaders.png"
+// import TicTacToe from "./tic-tac-toe.png"
 import ErrorGif from "./error-gif.gif"
 
 
 
 export default {
-    wavelengthEMR: WavelengthEMR,
-    spaceInvaders: SpaceInvaders,
-    ticTacToe: TicTacToe,
+    // wavelengthEMR: WavelengthEMR,
+    // spaceInvaders: SpaceInvaders,
+    // ticTacToe: TicTacToe,
     errorGif: ErrorGif
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,22 +9,20 @@ import { Link } from "react-router-dom";
 function Home(props) {
   return (
     <div className="home-ctr" id="home" >
-      {/* <HiOutlineComputerDesktop className="home-icon" /> */}
-      {/* <MdComputer className="home-icon" /> */}
+
       <div className="intro-container">
         <h3 className="intro">Hello world, I'm</h3>
         <TbDeviceImacCode className="home-icon" />
       </div>
       <h1 className="name">William Hu</h1>
       <h2 className="title">I am a <span className="typed-text"></span></h2>
-      {/* <h2 className="title input-wrapper typed-out">I am a <span className="input placeholder" >Software Developer</span></h2> */}
-      {/* <h3 className="greeting">Nice to e-meet you!</h3> */}
-      {/* <h4>and Registered Nurse</h4> */}
-      <div>
-        <Link to="/projects" >
-          <button><div className="flex-ctr-ctr" >Download my CV <HiDownload /></div></button>
-        </Link>
-      </div>
+      <button>
+        <a href="../../assets/files/william-hu-resume.pdf" download>
+          <div className="flex-ctr-ctr" >
+            Download my CV <HiDownload />
+          </div>
+        </a>
+      </button>
     </div>
   )
 }
