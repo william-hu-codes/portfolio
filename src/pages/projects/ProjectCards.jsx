03710d43aa1d904d7c7834d418f50a2ee6f113e8
william-hu-codes <william.hu307@gmail.com>
2023-09-11 19:40:06 -0400

    add project 3 to porjjects and add hover effect to projects to show tools used

--- src/pages/projects/ProjectCards.jsx
+++ src/pages/projects/ProjectCards.jsx
@@ -1,12 +1,19 @@
 export default function ProjectCards({project, idx}) {
-  const toolsList = project.tools.map((tool, index) => <p key={index} >{tool}</p>)
+  const toolsList = project.tools.map((tool, index) => <p key={index} className="tool">{tool}</p>)
 
     return (
         <div className="project-card" key={idx} >
           <div className="flex-ctr-ctr project-img-div">
             <img className="project-img" src={project.image} />
+            <div className="project-tools">
+              <h3 className="flex-ctr-ctr">Built with:</h3>
+              <div className="flex-ctr-ctr tools-list-ctr">
+                {toolsList}
+              </div>
+            </div>
           </div>
           <h2 className="flex-ctr-ctr" >{project.name}</h2>
+          <p className="flex-ctr-ctr description">{project.description}</p>
           <div className="buttons-div flex-ctr-ctr" >
             <a href={project.git}>
               <button>// GitHub</button>
